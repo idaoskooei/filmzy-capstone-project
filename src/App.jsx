6462aca6891268c535c1122efe2c065e48ce4d07
idaoskooei <idaoskooei@gmail.com>
2024-08-06 18:16:26 -0400

    add style to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import './App.scss'
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
+
 function App() {
   return (
-      <div className="App">
-          <Header />
-          <Footer />
+    <div className="App">
+      <Header />
+      <div className="content">
       </div>
+      <Footer />
+    </div>
   );
 }
-
 
 export default App;
