89cd5431137dcaabc48d478bb72ad34059d0ba72
idaoskooei <idaoskooei@gmail.com>
2024-08-06 17:29:45 -0400

    add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import './App.scss'
+import Footer from './components/Footer/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    </>
-  )
+      <div className="App">
+          <Footer />
+      </div>
+  );
 }
 
 export default App
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.scss';
+
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>© Filmzy Inc. All Rights Reserved.</p>
+        </footer>
+    );
+};
+  export default Footer;
