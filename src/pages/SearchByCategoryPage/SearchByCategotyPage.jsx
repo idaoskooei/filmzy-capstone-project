7d6c234dda8fb66b9d04f0080f1ac25e3da0744f
idaoskooei <idaoskooei@gmail.com>
2024-08-14 14:17:20 -0400

    fix styling issues across pages

--- src/pages/SearchByCategoryPage/SearchByCategotyPage.jsx
+++ src/pages/SearchByCategoryPage/SearchByCategotyPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Categories from '../../components/Categories/Categories'
+import './SearchByCategoryPage.scss';
+
 
 const SearchByCategoryPage = () => {
     return (
