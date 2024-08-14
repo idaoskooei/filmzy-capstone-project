9741143d5a798bef7927f4ec395b9e4aca4a3024
idaoskooei <idaoskooei@gmail.com>
2024-08-14 13:39:32 -0400

    final styling of random movie component

--- src/components/RandomMovie/RandomMovie.jsx
+++ src/components/RandomMovie/RandomMovie.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { FaCalendarAlt, FaClock, FaFilm, FaGlobe, FaLink, FaInfoCircle, FaLanguage } from 'react-icons/fa';
+
 import './RandomMovie.scss'; 
 
 const RandomMovie = () => {
@@ -18,7 +20,14 @@ const RandomMovie = () => {
       const movies = response.data.results;
       const randomMovie = movies[Math.floor(Math.random() * movies.length)];
 
-      setMovie(randomMovie);
+      const detailResponse = await axios.get(`https://api.themoviedb.org/3/movie/${randomMovie.id}`, {
+        params: {
+          api_key: 'b374a90d9ab89653cff28333dccd5836',
+          language: 'en-US'
+        }
+      });
+
+      setMovie(detailResponse.data);
     } catch (error) {
       console.error('Error fetching random movie:', error);
     } finally {
@@ -44,9 +53,12 @@ const RandomMovie = () => {
           className="movie-poster"
         />
         <div className="movie-info">
-          <h3 className="movie-title">{movie.title}</h3>
-          <p className="movie-release-date">{movie.release_date}</p>
-          <p className="movie-overview">{movie.overview}</p>
+          <p><FaFilm /> Title: {movie.title}</p>
+          <p><FaCalendarAlt /> Release Date: {movie.release_date}</p>
+          <p><FaClock /> Duration: {movie.runtime ? `${movie.runtime} mins` : 'Not available'}</p>
+          <p><FaLanguage /> Language: {movie.original_language}</p>
+          <p><FaLink /> Website: <a href={movie.homepage} target="_blank" rel="noopener noreferrer">{movie.homepage}</a></p>
+          <p><FaInfoCircle /> Overview: {movie.overview}</p>
         </div>
       </div>
       <button className="refresh-button" onClick={fetchRandomMovie}>Get New Recommendation</button>
