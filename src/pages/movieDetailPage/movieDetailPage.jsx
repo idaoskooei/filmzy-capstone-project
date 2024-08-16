33ac862235ab16fe276b78f9f17372a821a3ccd0
idaoskooei <idaoskooei@gmail.com>
2024-08-16 16:22:16 -0400

    rename

--- src/pages/movieDetailPage/movieDetailPage.jsx
+++ src/pages/movieDetailPage/movieDetailPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import MovieDetail from "../../components/MovieDetail/MovieDetail";
+import MovieDetail from "../../components/MovieDetailPage/MovieDetailPage";
 
 const MovieDetailPage = () => {
   const { movieId } = useParams();
